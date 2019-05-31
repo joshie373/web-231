08d3b4ec4b09031ed25ab0856c2c91838952fccb
Joshua Hughes <joshie373@gmail.com>
2019-05-30 20:50:49 -0700

    Commit 1.4

---
+++ week-1/hughes-assignment-1.4.js
@@ -0,0 +1,40 @@
+/* 
+============================================ 
+; Title: Assignment 1.4
+; Author: Joshua Hughes 
+; Date: 30 May 2019 
+; Modified By: Joshua Hughes 
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+
+//variable Declarations
+var firstName = "Joshua";
+var lastName =  "Hughes";
+var age = "29";
+var street = "1234 W. Test Street";
+var city = "Mesa";
+var zipCode = "85202";
+
+
+/*
+expected output:
+
+Joshua
+Hughes
+29
+1234 W. Test Street
+Mesa
+85202
+
+*/
+
+//output each variable to the console
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
