9519da01b35d4467b9c47524a641f2fc70127160
Joshua Hughes <joshie373@gmail.com>
2019-06-04 17:48:35 -0700

    adding week 2 header files

---
+++ week-2/hughes-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  hughes-header-test.js
+; Author: Joshua Hughes
+; Date:   04 June 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./hughes-header.js');
+
+console.log(header.display("Joshua", "Hughes", "Week 2"));
