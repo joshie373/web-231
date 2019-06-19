4a854b7c06a67623abc395585630f8484c4fb2dc
Joshua Hughes <joshie373@gmail.com>
2019-06-18 19:37:37 -0700

    adding 4.3 completed

--- week-4/hughes-4.3.js
+++ week-4/hughes-4.3.js
@@ -17,3 +17,35 @@ console.log(header.display("Joshua", "Hughes", "Exercise 4.3"));
 //line break
 console.log("\n");
 //===============header include======================
+
+//global variable definitions
+var vehicles = ["Car","Truck","Motorcycle","Boat","Train"];
+
+/**
+ * Params: array, string
+ * Response: elements of an array
+ * Description:  iterates over the parameterized array
+ * and outputs the results using the console.log() function.
+ * also output filtered string if present
+ */
+function getValue(array,string){
+  for (let i=0; i<array.length; i++){
+    if (!string) {
+      if(i==0){
+        console.log("-- DISPLAYING ARRAY ITEMS --");
+      }
+      console.log(array[i]);
+    }
+    else if(string==array[i]){
+      console.log("\n");
+      console.log("-- SELECTED VALUE --");
+      console.log(string);
+    }
+
+  }
+}
+
+//outputs
+getValue(vehicles);
+getValue(vehicles,"Train");
+getValue(vehicles,"Truck");
