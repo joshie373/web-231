f348fd99d0105daf6d2d59b828e17f628a8e1dd1
Joshua Hughes <hughes_josh6937@yahoo.com>
2019-07-03 14:04:06 -0700

    adding 6.2

---
+++ week-6/hughes-6.2.js
@@ -0,0 +1,36 @@
+/*
+============================================
+; Title:  hughes-5.2.js
+; Author: Joshua Hughes
+; Date:   3 July 2019
+; Description: exception handling
+;===========================================
+*/
+
+//===============header include======================
+//require header file
+const header = require('../hughes-header.js');
+
+//use header display function
+console.log(header.display("Joshua", "Hughes", "Exercise 6.2"));
+
+//line break
+console.log("\n");
+//===============header include======================
+
+
+
+try {
+    // variables
+    var x = "world";
+  
+    // if statement
+    if (isNaN(x)) throw 'NotaNumberException';
+  
+    // output
+    console.log(x + " " + y)
+  } catch (err) {
+    console.log('Catch clause: ' + err)
+  } finally {
+    console.log('Finally clause reached...')
+  }
